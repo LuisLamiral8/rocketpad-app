258e760fe035936230256e58586b1d188671c0d0
LuisLamiral8 <luislamiraal@gmail.com>
2022-01-26 19:53:00 -0300

    card-advance

--- pages/components/Article.jsx
+++ pages/components/Article.jsx
@@ -4,10 +4,44 @@ import Card from './../hooks/Card'
 const Article = () => {
   return(
       <article className='cards'>
-        <div className="input-container">
+        {/* <div className="input-container">
         <input id="search-btn" type="text" placeholder='Project name' />
-        </div>
+        </div> */}
+          
         <div className="cards-container">
+          <Card 
+          title= "Token1"
+          description= "Project in line to be loaded into the Rocketpad"
+          swapRate= "1 USDT = 2010.405 METAV"
+          cap= "10000"
+          access= "Private"
+          participants= "351"
+          />
+          {/* <Card 
+          title= "Token2"
+          description= "Project in line to be loaded into the Rocketpad"
+          swapRate= "1 USDT = 2010.405 METAV"
+          cap= "10000"
+          access= "Private"
+          participants= "351"
+          /> */}
+          {/*
+          <Card 
+          title= "Token3"
+          description= "Project in line to be loaded into the Rocketpad"
+          swapRate= "1 USDT = 2010.405 METAV"
+          cap= "10000"
+          access= "Private"
+          participants= "351"
+          />
+          <Card 
+          title= "Token4"
+          description= "Project in line to be loaded into the Rocketpad"
+          swapRate= "1 USDT = 2010.405 METAV"
+          cap= "10000"
+          access= "Private"
+          participants= "351"
+          /> */}
         </div>
 
      </article>
--- pages/hooks/Card.jsx
+++ pages/hooks/Card.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
 
-const Card = (title, description, swapRate, Cap, Access) => {
-  return(
-    <div className="card">
-        <div className='card_img-container'>
-            <img src="https://app.rocketpad.community/images/PLSPAD.jpg" alt="" />
-        </div>
-        <div className='card_info-container'>
-            <h2>{title}</h2>
-            <button>ig</button>
-            <button>fc</button>
-            <button>lnkdn</button>
-            <button>codepen</button>
-        </div>
-    </div>
+const Card = ({ title, description, swapRate, cap, access, participants }) => {
+    return (
+        <div className="card">
+            <div className="card_top-container">
+                <div className='card_img-container'>
+                    <img src="https://app.rocketpad.community/images/PLSPAD.jpg" alt="" width="55px" height="55px" />
+                </div>
 
-    ) 
+                <div className='card_info-container'>
+                    <h2>{title}</h2>
+                    <div className="social-media">
+                        <button>i</button>
+                        <button>f</button>
+                        <button>l</button>
+                        <button>c</button>
+                    </div>
+                    <div className="open-with">
+                    <a href="#"> <b className="item-open">⚬</b> Opens in TDA</a>
+                    <a href="#">ADA</a>
+                    </div>
+                </div>
+            </div>
+            <p className="card-description">{description}</p>
+            <div className="card-swapInfo">
+                <ul>
+                    <small class="swapInfo-small">Swap Rate <br /> <span className="swapInfo-data">{swapRate}</span></small>
+                    <small class="swapInfo-small">Cap <br /> <span className="swapInfo-data">{cap}</span>/</small>
+                    <small class="swapInfo-small">Access <br /> <span className="swapInfo-data">{access} </span></small>
+                </ul>
+            </div>
+            <div className="card-status">
+                <div>
+                <small>Progress</small>
+                <p>Participants:  <b>{participants}</b></p>
+                </div>
+
+                <div className="pp-card-progress">
+                    <div className="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    )
 };
 
 export default Card;
