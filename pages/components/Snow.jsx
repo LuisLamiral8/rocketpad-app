03d521d8de7fa44cc4241b80d323c57c3e1d723d
LuisLamiral8 <luislamiraal@gmail.com>
2022-01-26 20:10:44 -0300

    added-snow

---
+++ pages/components/Snow.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Snow = () => {
+  return(
+
+      <div>
+          <ul id="jsi-snows" class="g-snows">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
+      ) 
+};
+
+export default Snow;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Navbar from './components/Navbar'
 import Article from './components/Article'
 import Navigation from './components/Navigation.jsx'
-
+import Snow from './components/Snow'
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Navigation/>
       <Article/>
+      <Snow/>
     </div>
   )
 }
