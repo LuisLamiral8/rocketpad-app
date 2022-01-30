62d0be6d1cd12ca16aa7a2c891378d6bfd377286
Elian <elianpaludi@gmail.com>
2022-01-30 10:21:03 -0300

    cambios responsive

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -6,10 +6,19 @@ const Navbar = () => {
   if (typeof window !== 'undefined') {
     const ul = document.querySelector(".ul");
     const bars = document.querySelectorAll(".ham span")
+
     const responsiveNav = () => {
-      ul.classList.toggle("navActive");
-      bars.forEach(child => { child.classList.toggle('animated') });
+      // ul.classList.toggle("navActive");
+      // bars.forEach(child => { child.classList.toggle('animated') });
+
+      if(ul.classList.contains("navActive")){
+        ul.classList.remove("navActive");
+      }else{
+        ul.classList.add("navActive");
+      }
     }
+
+
   }
   return (
     <nav className="navbar">
@@ -18,8 +27,8 @@ const Navbar = () => {
         <li><Link href="https://rocketpad.community/">Home</Link></li>
         <li><label for="btn-modal" class="lbl-modal">Connect Wallet</label></li>
         <li><Link href="/">Projects</Link></li>
-        <li><Link href="/staking">Staking</Link></li>
-        <li><Link href="/tier-system" >Tier system</Link></li>
+        <li for="staking"><Link id="staking" href="/staking">Staking</Link></li>
+        <li for="tier"><Link id="tier" href="/tier-system" >Tier system</Link></li>
       </ul>
       <button onClick={() => responsiveNav()} className="ham" type="button">
         <span className="br-1"></span>
