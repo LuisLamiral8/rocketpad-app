0db64b78d8aaed1815ab0b344431ad0f82b0b6ee
LuisLamiral8 <luislamiraal@gmail.com>
2022-01-30 21:22:35 -0300

    tier-system page finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.scss'
 import '../styles/navbar.scss'
 import '../styles/card.scss'
-import '../styles/snow.scss'
 import '../styles/footer.scss'
 import '../styles/modal.scss'
 import '../styles/staking.scss'
 import '../styles/tiersystem.scss'
 import '../styles/tiers.scss'
+import '../styles/snow.scss'
 
 
 function MyApp({ Component, pageProps }) {
