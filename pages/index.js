f8934046d770bcec6d31edd453a64469afb48e2e
LuisLamiral8 <luislamiraal@gmail.com>
2022-02-02 18:20:08 -0300

    token pages responsive finished

--- pages/index.js
+++ pages/index.js
@@ -10,27 +10,27 @@ import Modal from './components/Modal'
 // Footer: Iconos
 export default function Home() {
 
-  if (typeof window !== 'undefined') {
- const cursor = document.getElementById('uno');
+//   if (typeof window !== 'undefined') {
+//  const cursor = document.getElementById('uno');
 
-  const handleMouseMove = (e) => {
-    cursor.style.top = e.clientY + 'px';
-    cursor.style.left = e.clientX + 'px';
-  }
+//   const handleMouseMove = (e) => {
+//     cursor.style.top = e.clientY + 'px';
+//     cursor.style.left = e.clientX + 'px';
+//   }
 
-  document.addEventListener('mousemove', handleMouseMove);
+//   document.addEventListener('mousemove', handleMouseMove);
 
 
-  const cursorDos = document.getElementById('dos');
+//   const cursorDos = document.getElementById('dos');
 
-  const handleMouseMoveDos = (e) => {
-    cursorDos.style.top = e.clientY + 'px';
-    cursorDos.style.left = e.clientX + 'px';
-  }
+//   const handleMouseMoveDos = (e) => {
+//     cursorDos.style.top = e.clientY + 'px';
+//     cursorDos.style.left = e.clientX + 'px';
+//   }
 
-  document.addEventListener('mousemove', handleMouseMoveDos);
+//   document.addEventListener('mousemove', handleMouseMoveDos);
 
-}
+// }
 
 
   return (
@@ -44,9 +44,9 @@ export default function Home() {
       <Footer/>
       <Snow/>
 
-      <div id="uno" className="mouse-cursor cursor-outer"></div>
-      <div id="dos" className="mouse-cursor cursor-inner"></div>
 
+      {/* <div id="uno" className="mouse-cursor cursor-outer"></div>
+      <div id="dos" className="mouse-cursor cursor-inner"></div>
       <style jsx>{`
       .mouse-cursor{
         position: fixed;
@@ -54,7 +54,7 @@ export default function Home() {
         top: 0;
         pointer-events: none;
         border-radius: 50%;
-        z-index:-5;
+        z-index:50 !important;
       }
 
       .cursor-outer{
@@ -68,18 +68,20 @@ export default function Home() {
         opacity: .5;
         -webkit-transition: all .08s ease-out;
         transition: all .08s ease-out;
+        z-index:50 !important;
       }
 
       .cursor-inner{
         margin-left: -3px;
       margin-top: -3px;
       width: 6px;
+      z-index:50 !important;
       height: 6px;
       background-color: #4d535e;
       -webkit-transition: width .3s ease-in-out, height .3s ease-in-out, margin .3s ease-in-out, opacity .3s ease-in-out;
       transition: width .3s ease-in-out, height .3s ease-in-out, margin .3s ease-in-out, opacity .3s ease-in-out;
       }
-  `}</style>
+  `}</style> */}
     </div>
   )
 }
